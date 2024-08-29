176c4b49383211a3ec6468e46010bf53504c90d8
panchanan-turuk <pturuk123@gmail.com>
2024-08-29 17:08:06 +0530

    HttpGet Implementation

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        //GET ALL REGIONS
+        // https://localhost:portnumber/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+           // var regions = dbContext.Regions.Find(id);
+            var regions = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regions == null)
+            {
+                return NotFound();
+
+            }
+
+            return Ok(regions);
+        }
+    }
+}
